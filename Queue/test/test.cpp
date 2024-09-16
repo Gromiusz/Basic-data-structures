3f11e0764eaddf4e418cb868895d31e3ca3d36e6
Gromiusz <kgorka22@gmail.com>
2024-09-16 11:02:28 +0200

    add the new kind of the constructor, refactor

--- Queue/test/test.cpp
+++ Queue/test/test.cpp
@@ -4,19 +4,19 @@
 TEST(Constructors, Constructors)
 {
     Deque<int> queue;
-    Deque<int> queue_ii(5, 4);
-    Deque<int> queue_i{3, 2};
+    Deque<int> queue_bracket(5, 4);
+    Deque<int> queue_buckle{3, 2};
 
     EXPECT_EQ(queue.size(), 0);
     EXPECT_TRUE(queue.empty());
 
-    EXPECT_EQ(queue_i.front(), 3);
-    EXPECT_EQ(queue_i.back(), 2);
-    EXPECT_EQ(queue_i.size(), 2);
-    EXPECT_FALSE(queue_i.empty());
+    EXPECT_EQ(queue_buckle.front(), 3);
+    EXPECT_EQ(queue_buckle.back(), 2);
+    EXPECT_EQ(queue_buckle.size(), 2);
+    EXPECT_FALSE(queue_buckle.empty());
 
-    EXPECT_EQ(queue_ii.front(), 4);
-    EXPECT_EQ(queue_ii.back(), 4);
-    EXPECT_EQ(queue_ii.size(), 5);
-    EXPECT_FALSE(queue_ii.empty());
+    EXPECT_EQ(queue_bracket.front(), 4);
+    EXPECT_EQ(queue_bracket.back(), 4);
+    EXPECT_EQ(queue_bracket.size(), 5);
+    EXPECT_FALSE(queue_bracket.empty());
 }
